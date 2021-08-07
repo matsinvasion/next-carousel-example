2ea5275019050d8f886252019cae8673fb4085ce
Musasizi <markmusasizi@gmail.com>
2021-08-07 20:49:34 +0300

    remove global css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import "../styles/globals.css";
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
