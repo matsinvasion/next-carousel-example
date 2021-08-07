2c9f99dbd0d023f1e8c60640ca577c945087c497
Musasizi <markmusasizi@gmail.com>
2021-08-07 21:44:39 +0300

    add css file

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ module.exports = {
     ...(process.env.NODE_ENV === 'production'
         ? {
              '@fullhuman/postcss-purgecss': {
-                content: ["/node_modules/react-responsive-carousel",
+                content: ["node_modules/react-responsive-carousel/lib/styles/carousel.min.css",
                  "./pages/**/*.js",
                   "./Componets/**/*.js"],
                 defaultExtractor: (content) =>
