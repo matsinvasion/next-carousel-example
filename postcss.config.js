2eeae0098a31b1ed7d6498d23d1109c4d894ad3f
Musasizi <markmusasizi@gmail.com>
2021-08-09 14:25:44 +0300

    add postcss-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,3 @@
-
 module.exports = {
-  plugins: {
-    ...(process.env.NODE_ENV === 'production'
-        ? {
-             '@fullhuman/postcss-purgecss': {
-                content: ["node_modules/react-responsive-carousel/lib/**/*.css",
-                 "./pages/**/*.js",
-                  "./Componets/**/*.js"],
-                  defaultExtractor: content => {
-                    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-                    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
-                    return broadMatches.concat(innerMatches)
-                  },
-             },
-          }
-        : {}),
-    tailwindcss: {},
-    autoprefixer: {},
-  }
-}
+  plugins: ["tailwindcss", "postcss-preset-env"]
+};
